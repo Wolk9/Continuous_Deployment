bb11a215bedbad9c487157bb1df59f112c53564a
Martin op iMac <wolk9@me.com>
2023-10-29 16:13:35 +0100

    cd

---
+++ main.py
@@ -0,0 +1,14 @@
+# Import what we need from flask
+from flask import Flask
+
+# Create a Flask app inside `app`
+app = Flask(__name__)
+
+# Assign a function to be called when the path `/` is requested
+@app.route('/')
+def index():
+    return 'Hello, world!'
+
+@app.route('/cow')
+def cow():
+    return 'MOoooOo!'
